dba4f9afe3e285fa38590b5a01de063d6a437629
JohnEricsson9 <johnericsson9@gmail.com>
2025-04-29 16:45:33 -0300

    Delete scripts directory

--- scripts/app.js
+++
@@ -1,5 +0,0 @@
-
-function showContent(id) {
-    document.querySelectorAll('.modulo-content').forEach(el => el.classList.add('hidden'));
-    document.getElementById(id).classList.remove('hidden');
-}
